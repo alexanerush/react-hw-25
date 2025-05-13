66b9714e482d2c15a36842b134ec8fb9e07b6cfc
Aliaksandra Nerush <aliaksandra.nerush@student.ehu.lt>
2025-05-13 19:57:29 +0300

    Completed the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import MenuPage from './pages/MenuPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartCount: 0,
-    };
-  }
+function App() {
+  const [cartCount, setCartCount] = useState(0);
 
-  handleAddToCart = () => {
-    this.setState(prevState => ({
-      cartCount: prevState.cartCount + 1,
-    }));
+  const handleAddToCart = () => {
+    setCartCount(prevCount => prevCount + 1);
   };
 
-  render() {
-    return (
-      <div className="app">
-        <div className="app-container">
-          <Header cartCount={this.state.cartCount} />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route
-              path="/menu"
-              element={<MenuPage onAddToCart={this.handleAddToCart} />}
-            />
-          </Routes>
-          <Footer />
-        </div>
+  return (
+    <div className="app">
+      <div className="app-container">
+        <Header cartCount={cartCount} />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route
+            path="/menu"
+            element={<MenuPage onAddToCart={handleAddToCart} />}
+          />
+        </Routes>
+        <Footer />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
-
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,77 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuCard from '../components/Card';
-import Button from '../components/Button'; 
+import Button from '../components/Button';
 import './MenuPage.css';
 
-class MenuPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      visibleCount: 6,
-    };
-  }
+const MenuPage = ({ onAddToCart }) => {
+  const [products, setProducts] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(6);
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals')
-      .then((res) => res.json())
-      .then((data) => this.setState({ products: data }))
-      .catch((err) => console.error('Error fetching meals:', err));
-  }
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error('Error fetching meals:', err));
+  }, []);
 
-  handleSeeMore = () => {
-    this.setState((prevState) => ({
-      visibleCount: prevState.visibleCount + 6,
-    }));
+  const handleSeeMore = () => {
+    setVisibleCount(prevCount => prevCount + 6);
   };
 
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setVisibleCount(6);
+  };
 
-  render() {
-    const { products, visibleCount } = this.state;
-    const visibleItems = products.slice(0, visibleCount);
+  const filteredProducts =
+    selectedCategory === 'All'
+      ? products
+      : products.filter(product => product.category === selectedCategory);
 
-    return (
-      <>
-        <div className="menu-page">
-          <div className="menu-container">
-            <h1 className="menu-title">Browse our menu</h1>
-            <p className="menu-subtitle">
-              Use our menu to place an order online, or{' '}
-              <span className="tooltip">
-                <span className="highlight">phone</span>
-                <span className="tooltip-text">+3706543786</span>
-              </span>{' '}
-              our store to place a pickup order. Fast and fresh food.
-            </p>
+  const visibleItems = filteredProducts.slice(0, visibleCount);
 
-            <div className="menu-filters">
-              {['Dessert', 'Dinner', 'Breakfast'].map((category) => (
-                <button key={category} className="filter-button" disabled>
-                  {category}
-                </button>
-              ))}
-            </div>
+  return (
+    <div className="menu-page">
+      <div className="menu-container">
+        <h1 className="menu-title">Browse our menu</h1>
+        <p className="menu-subtitle">
+          Use our menu to place an order online, or{' '}
+          <span className="tooltip">
+            <span className="highlight">phone</span>
+            <span className="tooltip-text">+3706543786</span>
+          </span>{' '}
+          our store to place a pickup order. Fast and fresh food.
+        </p>
 
-            <div className="item-cards">
+        <div className="menu-filters">
+          {['Dessert', 'Dinner', 'Breakfast'].map((category) => (
+            <button
+              key={category}
+              className="filter-button"
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+
+        <div className="item-cards">
           {visibleItems.map(product => (
             <MenuCard
               key={product.id}
               product={product}
-              onAddToCart={this.props.onAddToCart}
+              onAddToCart={onAddToCart}
             />
           ))}
         </div>
 
-        {visibleCount < products.length && (
-          <Button className="button--more" onClick={this.handleSeeMore}>
+        {visibleCount < filteredProducts.length && (
+          <Button className="button--more" onClick={handleSeeMore}>
             See More
           </Button>
         )}
       </div>
     </div>
-      </>
-    );
-  }
-}
+  );
+};
 
 export default MenuPage;
